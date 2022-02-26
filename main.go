1ed5c9dc5249fad3b42d552ecfd85ebea79f8ec5
Utsav2408 <utsavvora0809@gmail.com>
2022-02-26 12:12:11 +0530

    completed beginner course

--- main.go
+++ main.go
@@ -3,31 +3,34 @@ package main
 import (
 	"booking-app/helper"
 	"fmt"
-	"strconv"
+	"time"
 )
 
 const conferenceTickets int = 50
 
 var conferenceName string = "Go Conference"
 var remainingTickets uint = 50
-var bookings = make([]map[string]string, 0)
+
+type UserData struct {
+	userFirstName string
+	userLastName  string
+	userEmail     string
+	userTickets   uint
+}
+
+var bookings = make([]UserData, 0)
 
 func main() {
 	greetUsers()
 	for remainingTickets > 0 && len(bookings) < 50 {
-
 		userFirstName, userLastName, userEmail, userTickets := getUserInput()
 		isValidName, isValidEmail, isValidTicketNumber := helper.ValidateUserInput(userFirstName, userLastName, userEmail, userTickets, remainingTickets)
-
 		if isValidName && isValidEmail && isValidTicketNumber {
-
 			bookTicket(userTickets, userFirstName, userLastName, userEmail)
-			// call function for preprocess
+			go sendTicket(userTickets, userFirstName, userLastName, userEmail)
 			firstNames := getFirstNames()
 			fmt.Printf("The first names of bookings are: %v\n", firstNames)
-			// noTicketsRemaining  := remainingTickets == 0
 			if remainingTickets == 0 {
-				// end program
 				fmt.Println("Our conference is booked out. Comeback next year.")
 				break
 			}
@@ -52,10 +55,9 @@ func greetUsers() {
 }
 
 func getFirstNames() []string {
-	//For Each Loop
 	firstNames := []string{}
 	for _, booking := range bookings { // _ is a blank identifier. It marks those variables that are not used
-		firstNames = append(firstNames, booking["userFirstName"])
+		firstNames = append(firstNames, booking.userFirstName)
 	}
 	return firstNames
 }
@@ -65,33 +67,34 @@ func getUserInput() (string, string, string, uint) {
 	var userLastName string
 	var userEmail string
 	var userTickets uint
-	// ask user for their name
 	fmt.Println("Enter your first name: ")
 	fmt.Scan(&userFirstName)
-
 	fmt.Println("Enter your last name: ")
 	fmt.Scan(&userLastName)
-
 	fmt.Println("Enter your email address: ")
 	fmt.Scan(&userEmail)
-
 	fmt.Println("Enter number of tickets required: ")
 	fmt.Scan(&userTickets)
-
 	return userFirstName, userLastName, userEmail, userTickets
 }
 
 func bookTicket(userTickets uint, userFirstName string, userLastName string, userEmail string) {
 	remainingTickets -= userTickets
-
-	// create a map for a user
-	var userData = make(map[string]string)
-	userData["userFirstName"] = userFirstName
-	userData["userLastName"] = userLastName
-	userData["userEmail"] = userEmail
-	userData["userTickets"] = strconv.FormatUint(uint64(userTickets), 10)
-
+	var userData = UserData{
+		userFirstName: userFirstName,
+		userLastName:  userLastName,
+		userEmail:     userEmail,
+		userTickets:   userTickets,
+	}
 	bookings = append(bookings, userData)
 	fmt.Printf("Thankyou %v %v for booking %v tickets. You will receive a confirmation email at %v\n", userFirstName, userLastName, userTickets, userEmail)
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+}
+
+func sendTicket(userTickets uint, userFirstName string, userLastName string, userEmail string) {
+	time.Sleep(5 * time.Second)
+	var ticket = fmt.Sprintf("%v tickets for %v %v", userTickets, userFirstName, userLastName)
+	fmt.Println("########")
+	fmt.Printf("Sending ticket: \n %v \nto email address %v\n", ticket, userEmail)
+	fmt.Println("########")
 }
